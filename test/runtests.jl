4a84f9cad284ad348e12273adb97d85f3f2aa6c9
MilesCranmer <miles.cranmer@gmail.com>
2023-06-22 10:27:12 -0400

    Add tests to deprecate keywords

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,21 @@
 using Test
-using SafeTestsets
 using DeprecateKeyword
 
+@deprecate_kws (a=b,) function f(; a=2)
+    a
+end
+
+@test f(a=1) === 1
+@test_warn "Keyword argument `b` is deprecated. Use `a` instead." (@test f(b=1) == 1)
+
+@test_broken f(b=nothing) === nothing
+
+@deprecate_kws (α=β, γ=δ) function g(; α=2, γ=4)
+    α + γ
+end
+
+@test g() === 6
+@test g(α=1, γ=3) === 4
+@test_warn "Keyword argument" (@test g(β=1, γ=3) === 4)
+@test_warn "Keyword argument" (@test g(α=1, δ=3) === 4)
+@test_warn "Keyword argument `β`" (@test g(β=1, δ=3) === 4)
