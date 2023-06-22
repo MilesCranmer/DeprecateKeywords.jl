5dbefa475ad55b0bdbfa4d9a92cfbaa51247280e
MilesCranmer <miles.cranmer@gmail.com>
2023-06-22 10:53:00 -0400

    Ensure coverage is parallel

--- coverage.jl
+++ coverage.jl
@@ -1,7 +1,6 @@
 using Coverage
 # process '*.cov' files
 coverage = process_folder() # defaults to src/; alternatively, supply the folder name as argument
-push!(coverage, process_folder("ext/")...)
 
 LCOV.writefile("lcov.info", coverage)
 
@@ -9,7 +8,7 @@ LCOV.writefile("lcov.info", coverage)
 coverage = merge_coverage_counts(
     coverage,
     filter!(
-        let prefixes = (joinpath(pwd(), "src", ""), joinpath(pwd(), "ext", ""))
+        let prefixes = (joinpath(pwd(), "src", ""),)
             c -> any(p -> startswith(c.filename, p), prefixes)
         end,
         LCOV.readfolder("test"),
